cba7c5f239552fdd37ab1bfab57f79598b9a2768
Zayne Komchi <54587124+zaynekomichi@users.noreply.github.com>
2023-07-10 12:25:39 +0200

    onboarding changes

--- src/app/User/Auth/onboarding/page.tsx
+++ src/app/User/Auth/onboarding/page.tsx
@@ -32,17 +32,23 @@ export default function Register(){
             SetProgress(100)
         }
     }
+
+    const removeProgress=()=>{
+
+    }
     
 
     return(
         <div className={mp}>
             
            
-            <div className={vp}>
-                <div className="row" hidden={hide.hideOne}>
-                <div className="">
-            <h1 className="text-center ">Onboarding</h1>
+            <div className="container d-flex flex-column  justify-content-center align-items-center" style={{minHeight:"80vh"}}>
+            <div className="text-center mb-3">
+            <h1 >Onboarding</h1>
+            <p>Please fill in the necessary details to complete account registration!</p>
             </div>
+                <div className="row container" hidden={hide.hideOne}>
+               
                     <div className="col-sm">
                         <input type="text" className={genFrm} placeholder="Company Name*" onChange={(e)=>setName(e.target.value)} required/>
                     </div>
@@ -52,28 +58,35 @@ export default function Register(){
                     <div className="col-sm">
                         <input type="text" className={genFrm} placeholder="Address*" onChange={(e)=>setAddress(e.target.value)} required/>
                     </div>
-                    <button className={genBtn} onClick={()=>updateProgress()}>Next</button>
+                   
                 </div>
                 <div className="row" hidden={hide.hideTwo}>
                     <div className="col-sm">
                         <input type="number" className={genFrm} placeholder="Number of Employees*" onChange={(e)=>setNumEmployees(e.target.value)} required/>
                     </div>
                     <div className="col-sm">
-                        <input type="file" className={genFrm} placeholder="Company Registration Document CR5*" onChange={(e:any)=>setDoc(e.target.files[0])} required/>
+                        <span className="input-group-text">CR5 Document</span>
+                        <input type="file"  accept="application/pdf" className="form-control" placeholder="Company Registration Document CR5*" onChange={(e:any)=>setDoc(e.target.files[0])} required/>
                     </div>
-                    <div className="col-sm">
-                        <input type="text" className={genFrm} placeholder="Tax Clearence Certificate*" onChange={(e:any)=>setDoc(e.target.files[0])} required/>
+                    <div className="col-sm input-group">
+                        <span className="input-group-text">Tax Clearence</span>
+                        <input type="file" accept="application/pdf" className="form-control" placeholder="Tax Clearence Certificate*" onChange={(e:any)=>setDoc(e.target.files[0])} required/>
                     </div>
-                    <button className={genBtn} onClick={()=>updateProgress()}>Next</button>
+                   
                 </div>
                 <div className="row" hidden={hide.hideThree}>
                     <div className="col-sm">
                         <p>Choose up to five items</p>
                         <input type="text" placeholder="Category*" />
-                        <button className={genBtn} onClick={()=>updateProgress()}>Next</button>
+                       
                     </div>
                 </div>
-                <ProgressBar defaultValue={20} className="Bg"  now={progress}/>
+                <div className=" d-flex justify-content-between w-75  mb-3">
+                    <button className="btn Bg rounded-pill text-white">Back</button>
+                    
+                    <button className="btn Bg rounded-pill text-white">Next</button>
+                </div>
+                <progress className="Bg w-50 rounded-pill mt-5" value={progress} max={100}></progress>
             </div>
         </div>
     )
